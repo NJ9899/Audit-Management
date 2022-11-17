0401edf72e05308fa5f2739e18b35a9bf3c7de21
ustjavafsdb209 <namithajoseph99@gmail.com>
2022-11-17 11:39:16 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ChecklistComponent } from './checklist/checklist.component';
+import { AuditStatusComponent } from './audit-status/audit-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChecklistComponent,
+    AuditStatusComponent
   ],
   imports: [
     BrowserModule,
